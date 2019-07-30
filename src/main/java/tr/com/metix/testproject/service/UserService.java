0c7d2d67332bfdb45d8b20fc6803de45190cde20
Burcu Bağ <burcu.bag@metix.com.tr>
2019-07-30 11:18:53 +0300

    gitTest

--- src/main/java/tr/com/metix/testproject/service/UserService.java
+++ src/main/java/tr/com/metix/testproject/service/UserService.java
@@ -37,6 +37,7 @@ public class UserService {
     private final Logger log = LoggerFactory.getLogger(UserService.class);
 
     private final UserRepository userRepository;
+    
 
     private final PasswordEncoder passwordEncoder;
 
